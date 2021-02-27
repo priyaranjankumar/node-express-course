91178c54dcfbc743257efb9904c838f3a81c4c9d
PriyaRanjan Kumar <mishrasubham00@gmail.com>
2021-02-27 15:36:52 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+// adding the data json
+const mockUserData = [{ 'name': 'mark' }, { 'name': 'jill' }]
+
+
 // get the express library from node_modules
 
 const express = require('express')
 // create an instance of express constructor
 const app = express()
+// now we can access ethods from the express
 
-// now we can access methods from the express
+// creating a endpoint
+app.get('/users', (req, res) => {
+    res.json({
+        'success': true,
+        'message': 'successfully got the users. Nice!',
+        'users': mockUserData
+    })
+})
 
+
+// starting the server
 app.listen(8000, () => {
     console.log('Server is running at 8000');
 })
+
